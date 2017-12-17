31aa7c34eea1a73db9e5a538335e12243db44ba4
Santosh <santosharakere@gmail.com>
2017-12-17 18:14:48 +0000

    person component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I am a React App</h1>
-    //   </div>
-    // );
-    return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does it work now?'));
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App</h1>
+        <p>This is really working!</p>
+        <Person />
+        <Person />
+        <Person />
+        <Person />
+      </div>
+    );
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does it work now?'));
   }
 }
 
