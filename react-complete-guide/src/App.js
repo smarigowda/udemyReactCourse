69379f022525c033981ba275efa58a56d562a2f9
Santosh <santosharakere@gmail.com>
2017-12-18 06:21:33 +0000

    updating state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,15 +3,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [ 
+      { name: 'Santosh', age: 43 },
+      { name: 'Roopa', age: 40 },
+      { name: 'Sukruthi', age: 12 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS this.state.persons[0].name = 'Santosh Marigowda';
+    this.setState({
+      persons: [
+        { name: 'Santosh Marigowda', age: 43 },
+        { name: 'Roopa', age: 40 },
+        { name: 'Sukruthi', age: 13 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <button onClick={ this.switchNameHandler }>Switch Name</button>
+        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }/>
+        <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }>Hobbies: Reading</Person>
+        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age }/>
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does it work now?'));
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const person = () => {
+const person = props => {
   return (
-    <p>I am a Person and I am { Math.floor(Math.random() * 30) } years old!</p>
+    <div>
+      <p>I am { props.name } and I am { props.age } years old!</p>
+      <p>{ props.children }</p>
+    </div>
   )
 }
 
