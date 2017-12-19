97a84834a45a5e42287cf2dd31feaca353570c13
Santosh <santosharakere@gmail.com>
2017-12-19 21:04:16 +0000

    inline style

--- assignment-1/src/UserInput/UserInput.js
+++ assignment-1/src/UserInput/UserInput.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './UserInput.css';
 
 const userInput = props => {
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '2px solid green',
+    padding: '8px',
+    cursor: 'pointer',
+  };
+
   return (
     <div className="user-input">
-      <input type="text" onChange={props.changed} value={props.name}></input>
+      <input type="text" onChange={props.changed} value={props.name} style={style}></input>
     </div>
   )
 }
