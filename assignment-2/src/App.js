7b311c85f8d3489c1cc7a436868e2e9141b2b355
Santosh <santosharakere@gmail.com>
2017-12-22 05:56:49 +0000

    validation message

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -15,6 +15,8 @@ class App extends Component {
 
   }
 
+
+
   render() {
     return (
       <div className="App">
--- assignment-2/src/ValidationComponent/ValidationComponent.js
+++ assignment-2/src/ValidationComponent/ValidationComponent.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const validationComponent = props => {
+  let output = null;
+
+  if(props.charCount < 5) {
+    output = <p>Text is too short</p>
+  } else if (props.charCount > 25) {
+    output = <p>Text is too long</p>
+  }
+
   return (
     <div className="user-output">
       <p>Char Count = {props.charCount}</p>
+      {output}
     </div>
   )
 }
