54eb830b8162a5329243eb38832e982f479c8891
Santosh <santosharakere@gmail.com>
2017-12-24 05:25:07 +0000

    dynamic style

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -40,7 +40,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -63,6 +64,8 @@ class App extends Component {
           })}
         </div>
       )
+
+      style.backgroundColor = 'red';
     }
 
     return (
