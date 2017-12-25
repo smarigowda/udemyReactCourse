c7a454a6b54bcea7d57472a0a311a8652dec2352
Santosh <santosharakere@gmail.com>
2017-12-25 15:31:33 +0000

    this.props

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './Cockpit.css';
 
-const cockpit = (props) => {
+const cockpit = props => {
   let btnClass = '';
   if(props.showPersons) {
     btnClass = classes.red;
@@ -18,7 +18,7 @@ const cockpit = (props) => {
 
   return (
     <div className={classes.Cockpit}>
-      <h1>Hi, I am a React App</h1>
+      <h1>{props.appTitle}</h1>
       <p className={assignedClasses.join(' ')}>This is really working!</p>
       <button
         className={btnClass}
