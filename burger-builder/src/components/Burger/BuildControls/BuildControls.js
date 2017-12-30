c8f8b9ce6837346febdf23bc9726e0c99c101260
Santosh <santosharakere@gmail.com>
2017-12-30 03:35:35 +0000

    Less and More functionality implemented

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,13 @@ const buildControls = props => (
   <div className={classes.BuildControls}>
     {
       controls.map(ctrl => (
-        <BuildControl key={ctrl.label} label={ctrl.label} />
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          // type={ctrl.type}
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientsRemoved(ctrl.type)}
+        />
       ))
     }
   </div>
