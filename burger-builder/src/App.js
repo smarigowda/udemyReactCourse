e92d9195ba079b457c15199ab118bf3398cea82a
Santosh <santosharakere@gmail.com>
2018-01-06 10:04:34 +0000

    demo of unmounting withError component

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
+  state = {
+    show: true
+  }
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({show: false});
+    }, 20000);
+  }
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          {this.state.show ? <BurgerBuilder /> : null}
         </Layout>
       </div>
     );
--- burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,13 +10,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
       }
       // componentDidMount = () => {
       componentWillMount = () => { // runs before the child components are rendered
-        axios.interceptors.request.use(req => {
+        this.reqInterceptor = axios.interceptors.request.use(req => {
           this.setState({ error: null });
           return req;
         });
-        axios.interceptors.response.use(res => res, error => {
+        this.respInterceptor = axios.interceptors.response.use(res => res, error => {
           this.setState({ error: error }) // sets the error state
         });
+      }
+
+      componentWillUnmount() {
+        console.log('[withErrorHandler] componentWillUnmount');
+        console.log(`will unmount ${this.reqInterceptor} ${this.respInterceptor}`);
+        axios.interceptors.request.eject(this.reqInterceptor);
+        axios.interceptors.response.eject(this.respInterceptor);
       }
       errorConfirmedHandler = () => {
         this.setState({error: null});
