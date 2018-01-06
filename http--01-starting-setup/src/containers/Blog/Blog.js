63967a7d47ded497f4657b77d3c9c88a37bf842a
Santosh <santosharakere@gmail.com>
2018-01-06 16:27:02 +0000

    styling active route complete

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import axios from '../../../src/axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -7,17 +7,29 @@ import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render () {
+        console.log(this.props);
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink
+                                to="/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={
+                                    {
+                                        color: '#fa923f',
+                                        textDecoration: 'underline'
+                                    }
+                                }>Home
+                            </NavLink></li>
+                            <li><NavLink to={{
+                                // pathname: `${this.props.match.url}/new-post`,
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>  
                         </ul>
                     </nav>
                 </header>
