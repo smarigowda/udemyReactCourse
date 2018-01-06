e3468581cdb79e0ea55a122f631fd3b620057c44
Santosh <santosharakere@gmail.com>
2018-01-06 10:15:46 +0000

    section 10 completed

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
-  state = {
-    show: true
-  }
-  componentDidMount = () => {
-    setTimeout(() => {
-      this.setState({show: false});
-    }, 20000);
-  }
+  // state = {
+  //   show: true
+  // }
+  // componentDidMount = () => {
+  //   setTimeout(() => {
+  //     this.setState({show: false});
+  //   }, 20000);
+  // }
   render() {
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
+          {/* {this.state.show ? <BurgerBuilder /> : null} */}
+          <BurgerBuilder />
         </Layout>
       </div>
     );
