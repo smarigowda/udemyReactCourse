bcf807895939154f00f8903779d3cb2279cd5cad
Santosh <santosharakere@gmail.com>
2018-01-06 17:05:48 +0000

    switching between routes

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import axios from '../../../src/axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -34,7 +35,10 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" component={NewPost}/>
+                <Switch>
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/:id" component={FullPost}/>
+                </Switch>
                 {/* <Route path="/" exact render={() => <h1>Home sweet home!</h1>}/> */}
                 {/* <Route path="/" render={() => <h1>Home sweet home...2!</h1>}/> */}
                 {/* <Posts /> */}
--- http--01-starting-setup/src/containers/Blog/Posts/Posts.js
+++ http--01-starting-setup/src/containers/Blog/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from '../../../../src/axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css'; // we are not using css modules in this project
@@ -34,12 +35,14 @@ class Posts extends Component {
     let posts = <p style={{textAlign: 'center'}}>Something went wrong !</p>;
     if(!this.state.error) {
         posts = this.state.posts.map(post => {
-            return (<Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => { this.postSelectedHandler(post.id)}}
-            />)
+            return (
+            <Link to={`/${post.id}`} key={post.id}>
+              <Post
+                  title={post.title}
+                  author={post.author}
+                  clicked={() => { this.postSelectedHandler(post.id)}}
+              />
+            </Link>)
         });
     }
     return (
