0b083d8d495093602e133eb5cd498c9bae13a1a3
Santosh <santosharakere@gmail.com>
2018-01-14 19:26:24 +0000

    orders page

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 class App extends Component {
   // state = {
   //   show: true
@@ -18,7 +19,8 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout}/>
-          <Route path="/" exact component={BurgerBuilder}/>
+            <Route path="/orders" component={Orders}/>
+            <Route path="/" exact component={BurgerBuilder}/>
           </Switch>
           {/* {this.state.show ? <BurgerBuilder /> : null} */}
           {/* <BurgerBuilder /> */}
---
+++ burger-builder/src/containers/Orders/Orders.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import 
+
+class Orders extends Component {
+  render() {
+    return (
+      <div>
+        <Order />
+        <Order />
+        <Order />
+      </div>
+    );
+  }
+}
+
+export default Orders;
