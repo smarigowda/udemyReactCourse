ac4ec961c5704a408f1ac71c3b51f040e49bdcb2
Santosh <santosharakere@gmail.com>
2018-01-24 06:52:56 +0000

    initial setup

--- redux--assignment-2-problem/src/index.js
+++ redux--assignment-2-problem/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
+import reducer from './store/reducer';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore();
+const store = createStore(reducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ redux--assignment-2-problem/src/store/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  persons: []
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+}
+
+export default reducer;
