739fcab8eb43914eb09e87f8d0e9bb730288b590
Santosh <santosharakere@gmail.com>
2018-01-28 07:45:04 +0000

    async actions using redux-thunk

--- redux-adv--01-start/src/store/actions/actions.js
+++ redux-adv--01-start/src/store/actions/actions.js
@@ -29,10 +29,18 @@ export const subtract = val => {
   }
 }
 
-export const storeResult = result => {
+export const saveResult = result => {
   return {
     type: STORE_RESULT,
     result: result
+  }
+}
+
+export const storeResult = result => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(saveResult(result));
+    }, 5000);
   }
 }
 
