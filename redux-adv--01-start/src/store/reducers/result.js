4c2e9c8b7b21d0a77ef148fc0909d392aee7e771
Santosh <santosharakere@gmail.com>
2018-01-28 17:53:10 +0000

    some more refactoring

--- redux-adv--01-start/src/store/reducers/result.js
+++ redux-adv--01-start/src/store/reducers/result.js
@@ -5,6 +5,10 @@ const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, { results: updatedArray })
+}
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.STORE_RESULT:
@@ -12,8 +16,7 @@ const reducer = ( state = initialState, action ) => {
                 results: state.results.concat({id: new Date(), value: action.result * 2 }) 
             });
         case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return updateObject(state, { results: updatedArray })
+            return deleteResult(state, action);
     }
     return state;
 };
