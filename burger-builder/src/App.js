413d697f439383ebedd445d4311055561f1b4c54
Santosh <santosharakere@gmail.com>
2018-02-04 19:05:29 +0000

    storing token and id in localStorage. Fixing issues with routing and logout

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 class App extends Component {
   // state = {
   //   show: true
@@ -15,6 +18,9 @@ class App extends Component {
   //     this.setState({show: false});
   //   }, 20000);
   // }
+  componentDidMount() {
+    this.props.autoSign();
+  }
   render() {
     return (
       <div>
@@ -35,4 +41,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    autoSign: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -23,14 +23,19 @@ export const authFail = error => {
 }
 
 export const logout = () => {
+  debugger;
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_LOGOUT,
   }
 }
 
-export const checkAuthTimeout = (expiresIn, logout) => {
+export const checkAuthTimeout = expiresIn => {
   return dispatch => {
     setTimeout(() => {
+      debugger;
       dispatch(logout());
     }, +expiresIn * 1000)
   }
@@ -56,8 +61,13 @@ export const auth = (email, password, isSignUp) => {
     authData, { 'Content-Type': 'applicaton/json' })
     .then(response => {
       console.log(response);
+      const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+      localStorage.setItem('token', response.data.idToken);
+      localStorage.setItem('expirationDate', expirationDate);
+      localStorage.setItem('userId', response.data.localId);
       dispatch(authSuccess(response.data.idToken, response.data.localId));
-      dispatch(checkAuthTimeout(response.data.expiresIn, logout));
+      debugger;
+      dispatch(checkAuthTimeout(response.data.expiresIn));
     })
     .catch(error => {
       console.log("---- error ----", error);
@@ -71,5 +81,28 @@ export const setAuthRedirectPath = path => {
   return {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path,
+  }
+}
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      debugger;
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if(expirationDate > new Date()) {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token));
+        const expiresIn = (expirationDate.getTime() - new Date().getTime()) / 1000;
+        debugger;
+        dispatch(checkAuthTimeout(expiresIn));
+        debugger;
+      } else {
+        debugger;
+        dispatch(logout());
+      }
+    }
   }
 }
